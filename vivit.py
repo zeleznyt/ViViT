bbc6faa323cc35058e6686951db5fa6a7110f77c
zeleznyt <tomas@zelezny.name>
2024-09-03 09:33:07 +0200

    add tubelet embedding

--- vivit.py
+++ vivit.py
@@ -10,63 +10,58 @@ torch.manual_seed(0)
 
 
 class SpatialTransformer(nn.Module):
-    def __init__(self):
+    def __init__(self, embed_dim=768, num_heads=12, num_layers=12, patch_size=16, tubelet_size=2, image_size=224):  # TODO: get right hyperparameters
         super(SpatialTransformer, self).__init__()
-        # Using a pretrained Vision Transformer model from torchvision
-        self.vit = vit_b_16(weights='ViT_B_16_Weights.DEFAULT')
-        self.vit.heads = nn.Identity()  # Remove the classification head
 
-    def _process_input(self, x: torch.Tensor) -> torch.Tensor:
-        n, c, h, w = x.shape
-        p = 16  # TODO
-        # torch._assert(h == self.image_size, f"Wrong image height! Expected {self.image_size} but got {h}!")
-        # torch._assert(w == self.image_size, f"Wrong image width! Expected {self.image_size} but got {w}!")
-        n_h = h // p
-        n_w = w // p
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.embed_dim = embed_dim
+        self.tubelet_size = tubelet_size
+        encoder_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=num_heads, batch_first=True)
+        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
 
-        conv_proj = nn.Conv2d(
-            in_channels=3, out_channels=768, kernel_size=16, stride=16
-        ).cuda()  # TODO
+        self.conv_proj = nn.Conv3d(
+            in_channels=3, out_channels=embed_dim, kernel_size=(tubelet_size, patch_size, patch_size),
+            stride=(tubelet_size, patch_size, patch_size)
+        )
 
-        # (n, c, h, w) -> (n, hidden_dim, n_h, n_w)
-        x = conv_proj(x)
-        # (n, hidden_dim, n_h, n_w) -> (n, hidden_dim, (n_h * n_w))
-        x = x.reshape(n, 768, n_h * n_w)  # TODO
+        self.cls_token = nn.Parameter(torch.randn(1, 1, embed_dim))  # TODO: randn vs zeros
 
-        # (n, hidden_dim, (n_h * n_w)) -> (n, (n_h * n_w), hidden_dim)
-        # The self attention layer expects inputs in the format (N, S, E)
-        # where S is the source sequence length, N is the batch size, E is the
-        # embedding dimension
-        x = x.permute(0, 2, 1)
-
-        return x
+        seq_length = (image_size // patch_size) ** 2
+        seq_length += 1  # For cls token
+        self.pos_embed = nn.Parameter(torch.empty(1, seq_length, embed_dim).normal_(std=0.02))  # from BERT
 
     def forward(self, x):
         b, t, c, h, w = x.shape
-        x = rearrange(x, 'b t c h w -> (b t) c h w')  # flatten batch and temporal dims
+        n = b*t
 
-        # Pass through the model
-        # torchvision ViT will take care of embeddings and cls token
-        # x = self.vit(x)
+        torch._assert(h == self.image_size, f"Wrong image height! Expected {self.image_size} but got {h}!")
+        torch._assert(w == self.image_size, f"Wrong image width! Expected {self.image_size} but got {w}!")
+        torch._assert(t % self.tubelet_size == 0, f"Wrong video length! {t} must be dividable by {self.tubelet_size}!")
 
-        # Reshape and permute the input tensor
-        x = self._process_input(x)
-        n = x.shape[0]
+        n_h = h // self.patch_size
+        n_w = w // self.patch_size
+
+        # Reshape input for Conv3d
+        x = rearrange(x, 'b t c h w -> b c t h w')
+        x = self.conv_proj(x)
+
+        # Merge batch+time and height+width dimensions
+        x = rearrange(x, 'b d t h w -> (b t) (h w) d')
 
         # Expand the class token to the full batch
-        class_token = nn.Parameter(torch.zeros(1, 1, 768)).cuda()  # TODO
-        batch_class_token = class_token.expand(n, -1, -1)
-        x = torch.cat([batch_class_token, x], dim=1)
+        cls_token = repeat(self.cls_token, '() np d -> n np d', n=int(n/self.tubelet_size))
+        x = torch.cat([cls_token, x], dim=1)
 
-        encoder_layer = nn.TransformerEncoderLayer(d_model=768, nhead=12, batch_first=True).cuda()  # TODO
-        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=12).cuda()  # TODO
+        # Add positional encoding
+        x += self.pos_embed[:, :(n_h*n_w) + 1]
 
         x = self.encoder(x)
 
-        # Classifier "token" as used by standard language architectures
+        # Get class token
         x = x[:, 0]
 
-        x = rearrange(x, '(b t) ... -> b t ...', b=b)  # unflatten batch and temporal dims
+        x = rearrange(x, '(b t) ... -> b t ...', b=b)  # Unflatten batch and temporal dims
 
         return x  # Return the class token (batch_size, n_frames, embed_dim)
 
@@ -77,7 +72,8 @@ class TemporalTransformer(nn.Module):
         encoder_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=num_heads, batch_first=True)
         self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
 
-        self.cls_token = nn.Parameter(torch.randn(1, 1, embed_dim))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, embed_dim))  # TODO: randn vs zeros
+        # TODO: what to use for positional embedding
         # self.pos_embed = nn.Parameter(
         #     torch.zeros(1, 100, embed_dim))  # Adjust the 100 according to your max sequence length
         self.pos_embed = nn.Parameter(torch.empty(1, seq_length + 1, embed_dim).normal_(std=0.02))  # from BERT
@@ -100,7 +96,12 @@ class TemporalTransformer(nn.Module):
 class ViViT(nn.Module):
     def __init__(self, config):
         super(ViViT, self).__init__()
-        self.spatial_transformer = SpatialTransformer()
+        self.spatial_transformer = SpatialTransformer(embed_dim=config['embed_dim'],
+                                                        num_heads=config['spatial_num_heads'],
+                                                        num_layers=config['spatial_num_layers'],
+                                                        patch_size=config['patch_size'],
+                                                        tubelet_size=config['tubelet_size'],
+                                                        image_size=config['image_size'])
         self.temporal_transformer = TemporalTransformer(embed_dim=config['embed_dim'],
                                                         num_heads=config['temporal_num_heads'],
                                                         num_layers=config['temporal_num_layers'],
@@ -132,20 +133,13 @@ def load_config(cfg_path):
 
 
 if __name__ == '__main__':
-    # Example usage
 
     config_path = 'config.yaml'
     config = load_config(config_path)
 
-    # batch_size = 2
-    # num_frames = 8
-    # num_channels = 3
-    # height = 224
-    # width = 224
-    # num_classes = 10
-
     data_config = config['data']
 
+    # Example usage
     video_data = torch.randn(data_config['batch_size'], data_config['num_frames'], data_config['num_channels'],
                              data_config['height'], data_config['width']).cuda()
     model = ViViT(config['model']).cuda()

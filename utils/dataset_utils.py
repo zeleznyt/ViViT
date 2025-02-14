d4a6318fb8ef14756f3dd867aa29e7b93e481453
zeleznyt <tomas@zelezny.name>
2025-02-14 17:34:00 +0100

    add tool for creating annotation file

--- utils/dataset_utils.py
+++ utils/dataset_utils.py
@@ -16,12 +16,12 @@ def get_video_filename(q_file, video_path):
         return None
 
 
-def create_annotation_file(video_path, eaf_path, output_file='annotations.json'):
+def create_metadata_file(video_path, eaf_path, output_file='metadata.json'):
     """
-    Create annotation file from list of videos and eaf files
+    Create metadata file from list of videos and eaf files
     :param video_path: Path to videos
     :param eaf_path: Path to eaf files
-    :param output_file: Output json file with annotations
+    :param output_file: Output json file with metadata
     :return: json contents
     """
     annot_list = []
@@ -43,21 +43,21 @@ def create_annotation_file(video_path, eaf_path, output_file='annotations.json')
 
     with open(output_file, 'w') as f:
         json.dump(annot_list, f)
-    print('Annotation file created.')
+    print('Metadata file created.')
     return annot_list
 
 
-def create_annotations_full_split(video_path, eaf_path, output_dir='/annotations'):
+def create_metadata_full_split(video_path, eaf_path, output_dir='/metadata'):
     """
-    Create train/val/test annotation files from list of videos and eaf files
+    Create train/val/test metadata files from list of videos and eaf files
     :param video_path: Path to videos
     :param eaf_path: Path to eaf files
-    :param output_dir: Output directory of the annotation split
+    :param output_dir: Output directory of the metadata split
     """
-    full_annotation_file=os.path.join(output_dir, 'annotations.json')
-    create_annotation_file(video_path, eaf_path, output_file=full_annotation_file)
-    split_by_RAVDAI_TVchannel(original_metadata=full_annotation_file)
-    print(f'Annotation file created in {output_dir}.')
+    full_metadata_file=os.path.join(output_dir, 'metadata.json')
+    create_metadata_file(video_path, eaf_path, output_file=full_metadata_file)
+    split_by_RAVDAI_TVchannel(original_metadata=full_metadata_file)
+    print(f'Metadata file created in {output_dir}.')
 
 
 def convert_to_serializable(obj):

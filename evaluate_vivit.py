7e4904a0abb71141eed4029bf0a2ae8d83132736
zeleznyt <tomas@zelezny.name>
2025-05-07 14:27:54 +0200

    update evaluation script (use per class eval)

--- evaluate_vivit.py
+++ evaluate_vivit.py
@@ -33,11 +33,6 @@ if __name__ == "__main__":
     assert data_config['dataset_type'] in ['one_class', 'stream'], f'Dataset type {data_config["dataset_type"]} not supported'
     if data_config['dataset_type'] == 'one_class':
         val_dataset = VideoDataset(data_config['test_meta_file'], CLASSES,
-                               # load_from_json=data_config['val_json'],
-                               # frame_sample_rate=data_config['frame_sample_rate'],
-                               # min_sequence_length=data_config['min_sequence_length'],
-                               # max_sequence_length=data_config['max_sequence_length'],
-                               # video_decoder=data_config['video_decoder'],)
                             load_from_json = data_config['val_json'],
                             frame_sample_rate = data_config['frame_sample_rate'],
                             min_sequence_length = data_config['min_sequence_length'],
@@ -46,12 +41,6 @@ if __name__ == "__main__":
                             normalize = data_config['normalize'],)
     elif data_config['dataset_type'] == 'stream':
         val_dataset = VideoStreamDataset(data_config['test_meta_file'], CLASSES,
-                                     # load_from_json=data_config['val_json'],
-                                     # frame_sample_rate=data_config['frame_sample_rate'],
-                                     # context_size=data_config['context_size'],
-                                     # overlap=data_config['context_size'],
-                                     # max_empty_frames=data_config['max_empty_frames'],
-                                     # video_decoder=data_config['video_decoder'],)
                             load_from_json = data_config['val_json'],
                             frame_sample_rate = data_config['frame_sample_rate'],
                             context_size = data_config['context_size'],

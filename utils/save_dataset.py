ef148a7bbc2d6d1dc076002b431349a85685f9fa
zeleznyt <tomas@zelezny.name>
2025-10-23 18:22:52 +0200

    save embeddings from spatial encoder

--- utils/save_dataset.py
+++ utils/save_dataset.py
@@ -1,10 +1,16 @@
-import numpy as np
-import json
 import time
 import random
-from dataset import VideoDataset, VideoStreamDataset
+import numpy as np
+import decord
+from dataset import VideoDataset, VideoStreamDataset, preprocess_video
 from utils.train_utils import *
 from train_vivit import create_balanced_subset
+import h5py
+from tqdm import tqdm
+from torchvision.models import vit_b_16
+from einops import rearrange, repeat
+import torch.nn as nn
+import torch
 
 CLASSES = ['studio', 'indoor', 'outdoor', 'předěl', 'reklama', 'upoutávka', 'grafika', 'zábava']
 
@@ -60,6 +66,53 @@ def save_dataset(data_config, dataset_split='train', output_path='dataset/', bal
     else:
         return dataset.save_dataset_to_json(os.path.join(output_path, f'{dataset_type}_{dataset_split}_data.json'))
 
+
+@torch.no_grad()
+def save_embeddings_to_h5(data_dir, output_file, encoder, batch_size=16, step=25, normalize=True):
+    """
+    Precompute per-frame embeddings and save them into .h5 file.
+
+    h5 file maps frame indices to embeddings: {video_name: {index: [embedding vector]}}
+
+    Args:
+        data_dir: VideoStreamDataset instance
+        output_file (str): directory where .h5 files will be stored
+        encoder (nn.Module): encoder producing (1, T, D) embeddings
+        batch_size (int): frames processed per batch
+        step (int): step size in video (25 for 1-second step for 25 FPS video)
+        normalize (bool): image normalization
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    encoder = encoder.to(device).eval()
+
+    video_list = [os.path.join(data_dir, v) for v in os.listdir(data_dir) if v.lower().endswith('.mp4')]
+    print(f"Found {len(video_list)} unique videos.")
+
+    with h5py.File(output_file, 'w') as h5f:
+        for video_path in tqdm(video_list, desc="Saving embeddings"):
+            video_name = os.path.splitext(os.path.basename(video_path))[0]
+            vr = decord.VideoReader(video_path, num_threads=4)
+            num_frames = len(vr)
+
+            grp = h5f.create_group(video_name)
+
+            frame_indices = list(range(0, num_frames, step))
+
+            for i in range(0, len(frame_indices), batch_size):
+                batch_indices = frame_indices[i:i+batch_size]
+                frames = vr.get_batch(batch_indices).asnumpy()
+
+                frames = preprocess_video(frames, normalize=normalize)
+
+                frames = frames.to(device)
+                frames = rearrange(frames, 'b h w c -> b c h w')
+                emb = encoder(frames)  # (B, D)
+                emb = emb.squeeze(0).cpu().numpy()
+
+                for idx, vec in zip(batch_indices, emb):
+                    grp.create_dataset(str(idx), data=vec)
+
+
 if __name__ == "__main__":
     # Process args and config
     args = parse_args()
@@ -68,8 +121,12 @@ if __name__ == "__main__":
     random.seed(config['training']['seed'])
     data_config = config['data']
 
-    save_dataset(data_config=data_config, dataset_split='train', output_path='dataset_split/', balanced_dataset=config['training']['balance_dataset'])
-    save_dataset(data_config=data_config, dataset_split='val', output_path='dataset_split/', balanced_dataset=config['training']['balance_dataset'])
-    save_dataset(data_config=data_config, dataset_split='test', output_path='dataset_split/', balanced_dataset=config['training']['balance_dataset'])
+    # save_dataset(data_config=data_config, dataset_split='train', output_path='dataset_split/', balanced_dataset=config['training']['balance_dataset'])
+    # save_dataset(data_config=data_config, dataset_split='val', output_path='dataset_split/', balanced_dataset=config['training']['balance_dataset'])
+    # save_dataset(data_config=data_config, dataset_split='test', output_path='dataset_split/', balanced_dataset=config['training']['balance_dataset'])
+    data_dir = "/storage/plzen4-ntis/projects/korpusy_cv/RAVDAI/data-240p/"
+    encoder = vit_b_16(weights='ViT_B_16_Weights.DEFAULT')
+    encoder.heads = nn.Identity()
+    save_embeddings_to_h5(data_dir, "/storage/plzen4-ntis/projects/korpusy_cv/RAVDAI/embeddings.h5", encoder, 224, 2, normalize=data_config['normalize'])
 
 

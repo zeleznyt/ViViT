7cfe235fb9cb37b5e1e56de1b6ce4d444b6fa6cc
zeleznyt <tomas@zelezny.name>
2025-12-02 12:15:50 +0100

    retain processed frame embeddings

--- predictor.py
+++ predictor.py
@@ -5,6 +5,9 @@ from einops import rearrange
 import numpy as np
 import time
 from utils.train_utils import *
+from torchvision.models import vit_b_16
+import torch.nn as nn
+import decord
 
 
 CLASSES = ['studio', 'indoor', 'outdoor', 'předěl', 'reklama', 'upoutávka', 'grafika', 'zábava']
@@ -19,45 +22,103 @@ class ViViTpredictor():
         self.config = config
         self.device = get_device()
         self.classes = CLASSES
+        self.window_length = 17
+        self.embed_dim = 768
+        self.embed_frames = torch.zeros(self.window_length, self.embed_dim, device=self.device)
+        self.filled = 0
+        self.frames = []
+
+        encoder = vit_b_16(weights=None).to(self.device)
+        encoder.heads = nn.Identity()
+        self.spatial_encoder = encoder.to(self.device).eval()
+
+        self.temporal_encoder = ViViT(model_config, use_only_embeddings=True).to(self.device).eval()
 
         num_classes = len(CLASSES)
         model_config['num_classes'] = num_classes
 
-        print('Loading model...')
-        self.model = ViViT(model_config).to(self.device)
-        # Move non-trainable mask to the device
-        self.model.temporal_transformer.cls_mask = self.model.temporal_transformer.cls_mask.to(self.device)
-
-        # Load the model
+        print('Loading checkpoint...')
         checkpoint = torch.load(checkpoint_path, weights_only=True, map_location=torch.device(self.device))
-        self.model.load_state_dict(checkpoint['model_state_dict'])
+        state_dict = checkpoint["model_state_dict"]
+
+        spatial_state = {}
+        for k, v in state_dict.items():
+            if k.startswith("spatial_transformer"):
+                new_key = k.replace("spatial_transformer.vit.", "")
+                spatial_state[new_key] = v
+
+        self.spatial_encoder.load_state_dict(spatial_state, strict=True)
+
+        temporal_state = {}
+        for k, v in state_dict.items():
+            if k.startswith("temporal_transformer") or k.startswith("classifier"):
+                temporal_state[k] = v
+
+        self.temporal_encoder.load_state_dict(temporal_state, strict=True)
+
         print(f'Model loaded on {self.device} device.')
 
-    def predict(self, video: list) -> int:
+    def predict(self, frame: np.ndarray) -> tuple[int, list[float]]:
         """
         Predicts a class of the middle frame of the video.
-        :param video: List of video frames (ndarrays):
-                        Expected length: 17
-                        Frame shape: height x width x channels (standard decord output)
-        :return: predicted_class (int)
+        :param frame: ndarrays frame:
+                      Frame shape: height x width x channels (standard decord output)
+        :return: predicted_class (int), probs (list):
         """
-        processed_video = preprocess_video(video, normalize=True)
-        processed_video = rearrange(np.stack(processed_video), 't h w c -> t c h w')
-        processed_video = torch.from_numpy(processed_video).float().to(self.device)
-        processed_video = processed_video.unsqueeze(0)
+        self.frames.append(frame)
 
-        prediction = self.model(processed_video,
-                           padding_mask=torch.tensor([False] * processed_video.shape[1]).unsqueeze(0).to(self.device))
-        predicted_class = prediction.argmax(dim=1)
-        return predicted_class
+        preprocessed_frame = preprocess_video(np.expand_dims(frame, axis=0), normalize=True)
+        preprocessed_frame = preprocessed_frame.to(self.device)
+        preprocessed_frame = rearrange(preprocessed_frame, 'b h w c -> b c h w')
+        with torch.no_grad():
+            emb = self.spatial_encoder(preprocessed_frame)  # (1, D)
+
+        if self.filled < self.window_length:
+            self.filled += 1
+        else:
+            print(self.filled)
+            self.frames.pop(0)
+
+        self.embed_frames = torch.roll(self.embed_frames, shifts=-1, dims=0)
+        self.embed_frames[-1] = emb
+
+        # Padding mask
+        seq_len = self.filled
+        pad_len = self.window_length - seq_len
+
+        # Create a boolean tensor for the padding mask
+        padding_mask = torch.cat([
+            torch.ones(pad_len, dtype=torch.bool),
+            torch.zeros(seq_len, dtype=torch.bool)
+        ]).to(self.device)
+
+        embed_frames, padding_mask = [t.unsqueeze(0) for t in (self.embed_frames, padding_mask)]
+        with torch.no_grad():
+            pred = self.temporal_encoder(embed_frames, padding_mask)
+
+        probs = torch.softmax(pred, dim=1)
+        predicted_class = probs.argmax(dim=1)
+        return predicted_class.item(), probs.squeeze(0).cpu().tolist()
 
     def class2string(self, cls):
         return self.classes[cls]
 
-    def predict_class(self, video):
-        cls = self.predict(video)
+    def predict_class_with_probs(self, video):
+        cls, probs = self.predict(video)
         predicted_class = self.class2string(cls)
-        return predicted_class
+        class_probs = dict(zip(self.classes, probs))
+        return predicted_class, class_probs
+
+
+def frame_to_timestamp(total_seconds):
+    sign = "-" if total_seconds < 0 else ""
+    total_seconds = abs(int(total_seconds))
+
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+
+    return f"{sign}{hours:02d}:{minutes:02d}:{seconds:02d}"
 
 
 if __name__ == '__main__':
@@ -67,8 +128,20 @@ if __name__ == '__main__':
     model_config = config['model']
     checkpoint_path = config['evaluation']['checkpoint']
     predictor = ViViTpredictor(model_config, checkpoint_path)
-    input_video = [np.random.rand(240, 426, 3) for _ in range(17)] # List of ndarrays
-    start_time = time.time()
-    result = predictor.predict_class(input_video)
-    print('Predicted class: ', result)
-    print('Time elapsed during inference:', time.time() - start_time)
+    # input_video = [np.random.rand(240, 426, 3) for _ in range(80)] # List of ndarrays
+
+    n_frames_to_load = 250
+    fps = 25
+    indices = list(range(0, n_frames_to_load * fps, fps))
+    video_path = '/media/zeleznyt/DATA/repo/ViViT/example_data_RAVDAI/ct24 2023-10-02 01.27.34_example.mp4'
+    vr = decord.VideoReader(video_path)
+    input_video = [vr[i].asnumpy() for i in indices]
+
+    for i, input_frame in enumerate(input_video):
+        start_time = time.time()
+        if i*fps >= 400:
+            print(i*fps)
+        result, probs = predictor.predict_class_with_probs(input_frame)
+        probs_rounded = {k: f"{v:.2f}" for k, v in probs.items()}
+        print(f'Predicted class at {frame_to_timestamp(i-8)}: {result}. All probabilities: {probs_rounded}')
+        print('Time elapsed during inference:', time.time() - start_time)
